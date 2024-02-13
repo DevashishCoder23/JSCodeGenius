6216530b45111f1f7ccfcb97dacaf35781760f9e
DevashishCoder23 <devashishsharma039@gmail.com>
2024-02-13 17:38:13 +0530

    factorial of number

---
+++ beginner.js
@@ -0,0 +1,15 @@
+// Question 1> => Write a function that takes a number as input and returns the factorial of that number.
+
+// function factorial(n) {
+//   if (n === 0 || n === 1) {
+//     return 1;
+//   } else {
+//     let result = 1;
+//     for (let i = 2; i <= n; i++) {
+//       result *= i;
+//     }
+//     return result;
+//   }
+// }
+
+// console.log(factorial(5));

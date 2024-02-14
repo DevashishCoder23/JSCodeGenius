40bc17ae6d6df66532f005a45e837afcd1ccfed0
DevashishCoder23 <devashishsharma039@gmail.com>
2024-02-14 19:06:05 +0530

    sorting in ascending and removing duplicates

--- beginner.js
+++ beginner.js
@@ -13,3 +13,55 @@
 // }
 
 // console.log(factorial(5));
+
+// Question 2> Write a function to sort an array of numbers in ascending order.
+
+// Solution 1> Without using built in method.
+// function ascendingOrder(arr){
+//     for(let i=0; i < arr.length-1 ; i++){
+//         for(let j=i+1; j < arr.length; j++){
+//             if(arr[i] > arr[j]){
+//                 let nextVal = arr[j];
+//                 arr[j] = arr[i];
+//                 arr[i] = nextVal;
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(ascendingOrder([1,122,12,1,4,0,90,14]));
+
+// Solution 2> By using Built in method.
+
+// function ascendingOrder(arr) {
+//   return arr.sort((a, b) => a - b);
+// }
+
+// console.log(ascendingOrder([22, 11, 9, 6, 0, 13, 55]));
+
+// Question 3> Removing duplicate elements from an array.
+
+// function removingDuplicateElem(arr) {
+//   let duplicateRemovedArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let isDuplicate = false;
+
+//     for (let j = 0; j < duplicateRemovedArr.length; j++) {
+//       if (arr[i] === duplicateRemovedArr[j]) {
+//         isDuplicate = true;
+//         break;
+//       }
+//     }
+//     if (!isDuplicate) {
+//       duplicateRemovedArr.push(arr[i]);
+//     }
+//   }
+//   return duplicateRemovedArr;
+// }
+
+// function removingDuplicateElem(arr) {
+//   return arr.filter((ele, index) => arr.indexOf(ele) === index);
+// }
+
+// console.log(removingDuplicateElem([1, 1, 1, 2, 2, 2, 3, 1, 3, 3, 4, 4, 4]));

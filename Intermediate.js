fbef2694477a8a075168653b89dacf3f8088ba00
DevashishCoder23 <devashishsharma039@gmail.com>
2025-04-15 22:37:37 +0530

    multiple problem solving questions.

--- Intermediate.js
+++ Intermediate.js
@@ -254,3 +254,269 @@
  
 //  console.log("RESULT ===>>",mergeSortedArr([0,3,4,31],[4,6,30]));
 
+// class NewNode{
+//     constructor(value){
+//         this.value = value;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
+
+// class BinarySearchTree{
+//     constructor(){
+//         this.root = null;
+//     }
+    
+//     shiftTo(currentNodeValue, newNodeValue){
+//         if(currentNodeValue < newNodeValue){
+//             return "right";
+//         }else{
+//             return "left";
+//         }
+//     }
+//     //     9
+//     //  7     20
+//     //1   6  15  170
+//     // 5
+//    //4 
+//     remove(value){
+//         if(this.root){
+//             let previousNode = null;
+//             let previousNodeDirection = null;
+//             let currentNode = this.root;
+            
+//             while(currentNode){
+//                 if(currentNode.value === value){
+//                     if(currentNode.right){
+//                      let firstRightNode = currentNode.right;
+//                      if(firstRightNode.left === null){
+//                         firstRightNode.left = currentNode.left;
+//                         previousNode[previousNodeDirection] = firstRightNode;
+//                      }else{
+//                          let RightNodeLeftPointerReference = firstRightNode.left;
+//                          while(RightNodeLeftPointerReference.left){
+//                              RightNodeLeftPointerReference = RightNodeLeftPointerReference.left;
+//                          }
+//                          RightNodeLeftPointerReference.right = currentNode.right;
+//                          RightNodeLeftPointerReference.left = currentNode.left;
+//                          RightNodeLeftPointerReference
+//                         previousNode[previousNodeDirection] = RightNodeLeftPointerReference;
+//                      }
+//                     }else if(currentNode.left){
+//                       previousNode[previousNodeDirection] = currentNode.left;
+//                     }else{
+//                         if(previousNodeDirection){
+//                          previousNode[previousNodeDirection] = null;
+//                         }else{
+//                          this.root = null    
+//                         }
+//                     }
+//                     // Need changes here
+//                     return currentNode;
+//                 }
+                
+                
+                
+//               const shiftToward = this.shiftTo(currentNode.value,value);
+//               const isNodeExist = currentNode[shiftToward];
+              
+//               if(isNodeExist){
+//                   previousNodeDirection = shiftToward;
+//                   previousNode = currentNode;
+//                   currentNode = isNodeExist;
+//               }else{
+//                               console.log("There is no node with this value. Nothing to remove.");
+//                  return "There is no node with this value. Nothing to remove."
+//               }
+//             }
+            
+//         }else{
+//             console.log("Nothing to remove, As Root node does not exist.");
+//             return "Nothing to remove, As Root node does not exist.";
+//         }
+//     }
+    
+    
+//     lookup(value){
+//         if(this.root){
+//             let currentNode = this.root;
+//             while(currentNode){
+//                 if(currentNode.value === value){
+//                     return currentNode;
+//                 }
+//               const shiftToward = this.shiftTo(currentNode.value,value);
+//               const isNodeExist = currentNode[shiftToward];
+//               if(isNodeExist){
+//                   if(isNodeExist.value === value){
+//                       return isNodeExist;
+//                   }else{
+//                      currentNode = isNodeExist;   
+//                   }
+//               }else{
+//                  return false
+//               }
+//             }
+            
+//         }else{
+//             if(this.root.value === value){
+//                 return this.root
+//             }else{
+//                 return false;
+//             }
+//         }
+//     }
+    
+//     // left and right node comparision then update node
+//     insert(value){
+//         let newNode = new NewNode(value);
+//         if(this.root){
+//           let currentNode = this.root;
+//           let isNodeUpdate = false;
+          
+//           while(!isNodeUpdate){
+//               const shiftToward = this.shiftTo(currentNode.value,newNode.value);
+//               const isNodeExist = currentNode[shiftToward];
+//               if(isNodeExist){
+//                   currentNode = isNodeExist;
+//               }else{
+//                  currentNode[shiftToward] = newNode;
+//                  isNodeUpdate = true;
+//               }
+//           }
+//         }else{
+//             this.root = newNode;
+//         }
+        
+//     }
+// }
+
+
+
+// const tree = new BinarySearchTree();
+// tree.insert(9);
+// tree.insert(4);
+// tree.insert(6);
+// tree.insert(20);
+// tree.insert(170);
+// tree.insert(15);
+// tree.insert(1);
+// tree.remove(1);
+// tree.remove(6);
+// tree.remove(15);
+// tree.remove(170);
+// tree.remove(4);
+// tree.remove(20);
+// tree.remove(9);
+// tree.insert(166);
+// tree.insert(160);
+// tree.insert(163);
+// console.log(tree)
+// tree.remove(170);
+// console.log(JSON.stringify(traverse(tree?.root)));
+// console.log(tree.lookup(166));
+//     9
+//  4     20
+//1  6  15  170
+
+// function traverse(node) {
+//   const tree = { value: node?.value };
+//   tree.left = node?.left === null ? null : traverse(node?.left);
+//   tree.right = node?.right === null ? null : traverse(node?.right);
+//   return tree;
+// }
+
+
+// Graph implementation => undirected Graph > unweighted Graph > implementation through > adjacement list > using hash table
+
+// adjacentlist, edges, adjacent matrix,
+// class Graph{
+//     constructor(){
+//        this.numberOfNodes = 0;
+//        this.adjacentList = {
+//        };
+//     }
+    
+//     addVertex(node){
+//         if(!this.adjacentList.hasOwnProperty(node)){
+//             this.adjacentList[node] =  [];
+//             this.numberOfNodes++;
+//         }
+//     }
+    
+//     addEdge(node1,node2){
+//         if(!this.adjacentList.hasOwnProperty(node1)){
+//             this.addVertex(node1);
+//         }
+//             this.adjacentList[node1] = [ ...this.adjacentList[node1], node2]   
+        
+//         if(!this.adjacentList.hasOwnProperty(node2)){
+//             this.addVertex(node2);
+//         }
+//             this.adjacentList[node2] = [ ...this.adjacentList[node2], node1]
+//     }
+    
+//     showConnections(){
+//         console.log(this.adjacentList)
+//         let output = "";
+//         let obj = this.adjacentList;
+//         let line = ""
+//         for(let key in obj){
+//              line += key + "-->";
+//              obj[key].forEach((item) => {
+//                  line += item + " ";
+//              });
+//              output += line + "\n";
+//              line = "";
+//         }
+//         console.log(output)
+//     }
+    
+// };
+
+// const myGraph = new Graph();
+//  myGraph.addVertex('0');
+// myGraph.addVertex('1');
+// myGraph.addVertex('2');
+// myGraph.addVertex('3');
+// myGraph.addVertex('4');
+// myGraph.addVertex('5');
+// myGraph.addVertex('6');
+// myGraph.addEdge('3', '1'); 
+// myGraph.addEdge('3', '4'); 
+// myGraph.addEdge('4', '2'); 
+// myGraph.addEdge('4', '5'); 
+// myGraph.addEdge('1', '2'); 
+// myGraph.addEdge('1', '0'); 
+// myGraph.addEdge('0', '2'); 
+// myGraph.addEdge('6', '5');
+// myGraph.showConnections();
+//Answer:
+// 0-->1 2 
+// 1-->3 2 0 
+// 2-->4 1 0 
+// 3-->1 4 
+// 4-->3 2 5 
+// 5-->4 6 
+// 6-->5
+
+// Time complexcity 2^n
+// function fibonacciSeriesRecursive(n){ 
+//     if(n < 2){
+//         return n;
+//     }
+    
+//     return fibonacciSeriesRecursive(n - 1) + fibonacciSeriesRecursive(n - 2);
+// };
+
+// console.log("RESULT ==>",fibonacciSeriesRecursive(44))
+
+// Time Complexity => o(n)
+// function fibonacciSeriesItratevily(n){
+//     let arr = [0,1];
+//     for(let i=2; i<=n; i++){
+//         arr.push(arr[i-1] + arr[i-2]);
+//     }
+//     return arr[n];
+// };
+// console.log("RESULT ==>",fibonacciSeriesItratevily(4));
+
